9445e781913936c06248f004c67810297dc26825
topolino <topolino@mail.com>
2021-08-05 10:48:33 -0300

    new infra project

--- iac.js
+++ iac.js
@@ -1,25 +1,27 @@
+const assert = require("assert");
 const { AwsProvider } = require("@grucloud/provider-aws");
-const ModuleAwsVpc = require("@grucloud/module-aws-vpc");
-const ModuleAwsEKS = require("@grucloud/module-aws-eks");
+const hook = require("./hook");
+
+const createResources = async ({ provider, resources: {} }) => {
+  const { stage } = provider.config;
+
+  return {};
+};
+exports.createResources = createResources;
 
 exports.createStack = async ({ createProvider }) => {
-  const provider = createProvider(AwsProvider, {
-    configs: [require("./config"), ModuleAwsVpc.config, ModuleAwsEKS.config],
-  });
+  const provider = createProvider(AwsProvider, { config: require("./config") });
+  const { stage } = provider.config;
+  assert(stage, "missing stage");
 
-  const vpcResources = await ModuleAwsVpc.createResources({
+  const resources = await createResources({
     provider,
-  });
-
-  const eksResources = await ModuleAwsEKS.createResources({
-    provider,
-    resources: vpcResources,
+    resources: {},
   });
 
   return {
     provider,
-    resources: { vpc: vpcResources, eks: eksResources },
-    hooks: [...ModuleAwsVpc.hooks, ...ModuleAwsEKS.hooks],
-    isProviderUp: () => ModuleAwsEKS.isProviderUp({ resources: eksResources }),
+    resources,
+    hooks: [hook],
   };
 };
--- test/iac.test.js
+++ test/iac.test.js
@@ -1,20 +1,14 @@
 const assert = require("assert");
-const path = require("path");
-const { ConfigLoader } = require("@grucloud/core/ConfigLoader");
 const { Cli } = require("@grucloud/core/cli/cliCommands");
 const { createStack } = require("../iac");
+const config = require("../config");
+const path = require("path");
 
-describe("EKS Module", async function () {
-  before(async function () {
-    try {
-      ConfigLoader({ path: "../../examples/multi" });
-    } catch (error) {
-      this.skip();
-    }
-  });
+describe("AWS Template project", async function () {
+  before(async function () {});
   it("run", async function () {
     const programOptions = { workingDirectory: path.resolve(__dirname, "../") };
-    const cli = await Cli({ programOptions, createStack });
+    const cli = await Cli({ programOptions, createStack, config });
 
     await cli.graphTree({
       commandOptions: {},
@@ -27,30 +21,19 @@ describe("EKS Module", async function () {
     await cli.planDestroy({
       commandOptions: { force: true },
     });
-    await cli.planQuery({});
     await cli.planApply({
       commandOptions: { force: true },
     });
-    await cli.planApply({
-      commandOptions: {},
-    });
-    await cli.planQuery({});
-    await cli.planRunScript({
-      commandOptions: { onDeployed: true },
+    await cli.list({
+      commandOptions: { our: true, graph: true },
     });
     await cli.planDestroy({
       commandOptions: { force: true },
-    });
-    await cli.planRunScript({
-      commandOptions: { onDestroyed: true },
-    });
-    await cli.planDestroy({
-      commandOptions: {},
     });
     // TODO list should be empty
     const result = await cli.list({
       commandOptions: { our: true },
     });
     assert(result);
-  }).timeout(35 * 60e3);
+  });
 });
