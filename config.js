dc5a602d0becd361fb3c53998bdac07b1f359f65
topolino <topolino@mail.com>
2021-08-23 20:06:43 -0300

    new infra project

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
-module.exports = () => ({
-  projectName: "aws-test",
-});
+const pkg = require("./package.json");
+
+module.exports = ({}) => {
+  return {
+    projectName: "ex-eks-mod",
+    //    formatName: (name, config) => `${name}-${config.projectName}`,
+    formatName: (name, config) => name,
+    eks: {
+      cluster: {
+        name: "my-cluster",
+      },
+    },
+  };
+};
