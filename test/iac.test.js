7c226e146601fa7d2caf512ecfd18f1f4078718f
topolino <topolino@mail.com>
2021-08-23 20:09:05 -0300

    new infra project

--- test/iac.test.js
+++ test/iac.test.js
@@ -1,26 +1,15 @@
 const assert = require("assert");
-const path = require("path");
-const { ConfigLoader } = require("@grucloud/core/ConfigLoader");
 const { Cli, testEnd2End } = require("@grucloud/core/cli/cliCommands");
 const { createStack } = require("../iac");
+const config = require("../config");
+const path = require("path");
 
-const title = "EKS Module";
-describe(title, async function () {
-  before(async function () {
-    try {
-      ConfigLoader({ path: "../../examples/multi" });
-    } catch (error) {
-      this.skip();
-    }
-  });
-
+describe("AWS Template project", async function () {
+  before(async function () {});
   it("run", async function () {
     const programOptions = { workingDirectory: path.resolve(__dirname, "../") };
-    const cli = await Cli({ programOptions, createStack });
+    const cli = await Cli({ programOptions, createStack, config });
 
-    await testEnd2End({
-      cli,
-      title,
-    });
-  }).timeout(35 * 60e3);
+    await testEnd2End({ cli });
+  });
 });
